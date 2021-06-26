284440882443013d21fc5143b9641797e1f77f80
brianslab <bdkuhn602@gmail.com>
2021-06-25 22:54:30 -0700

    deleted learn react link

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Fib Calc
-          </a>
+          <h1 className="App-title">Fib Calc</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
